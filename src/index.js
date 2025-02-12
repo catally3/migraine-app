f55656d87490643235090569d2bc886e78105a45
catally3 <68207926+catally3@users.noreply.github.com>
2025-02-12 12:04:02 -0600

    added connection to database to app code

--- src/index.js
+++ src/index.js
@@ -2,11 +2,38 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const pgp = require('pg-promise')()
+const cn = {
+  host: 'host.docker.internal',
+  port: 5434,
+  database: process.env.POSTGRES_DATABASE,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  max: 30 // use up to 30 connections
+
+  // "types" - in case you want to set custom type parsers on the pool level
+};
+
+const db = pgp(cn);
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/medications', async (req, res) => {
+  try {
+    const meds = await db.any('SELECT * FROM "migraine-app".medication');
+    res.send(meds);
+    // success
+  } 
+  catch(e) {
+    // error
+  }
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+ 
+
