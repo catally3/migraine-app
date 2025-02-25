55dedc59ea6df3c97161b4c64e82caf061830664
catally3 <68207926+catally3@users.noreply.github.com>
2025-02-25 13:24:15 -0600

    Added Post Statement
    I tried changing it around a bit but I am still getting the error about medicationName being undefined

--- src/index.js
+++ src/index.js
@@ -32,6 +32,19 @@ app.get('/medications', async (req, res) => {
   }
 })
 
+app.post('/medication', async (req, res) => {
+  const body = req.body;
+  // then extract your exact fields
+  const medicationName = body.medicationName;
+  const doseage = body.doseage;
+  const medicationType = body.medicationType
+  const newItem = await db.any("INSERT INTO \"migraine-app\".medication (name, doseage, type) VALUES ('$1', '$2', '$3')", 
+    [medicationName, doseage, medicationType]);
+   res.send({
+    newItem,
+  })
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
